b226838b2c5fd4a51ec2394b3573009e87acec72
lwooden <lowell.wooden@gmail.com>
2020-05-11 15:50:38 -0400

    added more datasets to database bootstrap function

--- server/boot/create-passage-model-data.js
+++ server/boot/create-passage-model-data.js
@@ -12,7 +12,23 @@ module.exports = async function (app) {
     passageText: 'For God so loved the world, that he gave His only Begotten Son....',
     passageLocation: 'John 3:16',
     categoryId: 1
-  }]);
+  }, {
+    passageText: 'Come to me, all you who are weary and burdened, and I will give you rest.',
+    passageLocation: 'Matthew 11:28-30',
+    categoryId: 1
+
+  },
+  {
+    passageText: 'Pride goes before destruction, a haughty spirit before a fall.',
+    passageLocation: 'Proverbs 16:18',
+    categoryId: 2
+  },{
+    passageText: 'When pride comes, then comes disgrace, but with humility comes wisdom.',
+    passageLocation: 'Proverbs 11:2',
+    categoryId: 2
+  }
+
+]);
 
   console.log('Data created: \n', columns);
 };
