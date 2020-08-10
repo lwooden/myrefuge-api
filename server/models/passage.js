d9b944f3f0ccf60dc56887187e54248b7fac01a6
lwooden <lowell.wooden@gmail.com>
2020-08-10 16:05:48 -0400

    add custom post endpoint to process likeCounter updates

--- server/models/passage.js
+++ server/models/passage.js
@@ -1,5 +1,31 @@
 'use strict';
 
 module.exports = function(Passage) {
+    Passage.likeVerse = async function(data) {
+        // Passage.findById(passageId, function (err, instance) {
+        //     var response = "The text of the verse is " + instance.passageText;
+        //     // callback(null, response);
+        //     console.log(response);
+        // });
+      
+
+        Passage.upsertWithWhere({id:data.passageId}, {likeCounter: data.currentLikeCounter}, function(err, obj) {
+            console.log(err)
+            console.log("Like Counter updated: ", obj)
+            
+        })
+
+      }
+
+      Passage.remoteMethod (
+            'likeVerse',
+            {
+              http: {path: '/likeVerse', verb: 'post'},
+              accepts: [
+                  {arg: 'data', type: 'object', http: { source: 'body' } }
+            ],
+              returns: {arg: 'name', type: 'string'}
+            }
+        );
 
 };
