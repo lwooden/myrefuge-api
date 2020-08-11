759289ee130108a335c51d45d77fa3d53ec5ffcb
lwooden <lowell.wooden@gmail.com>
2020-08-10 20:55:51 -0400

    update remote method

--- server/models/passage.js
+++ server/models/passage.js
@@ -2,18 +2,31 @@
 
 module.exports = function(Passage) {
     Passage.likeVerse = async function(data) {
-        // Passage.findById(passageId, function (err, instance) {
-        //     var response = "The text of the verse is " + instance.passageText;
-        //     // callback(null, response);
-        //     console.log(response);
-        // });
-      
+        Passage.findById(data.passageId, function (err, instance) {
+            // var response = "The text of the verse is " + instance.passageText;
+            // callback(null, response);
 
-        Passage.upsertWithWhere({id:data.passageId}, {likeCounter: data.currentLikeCounter}, function(err, obj) {
-            console.log(err)
-            console.log("Like Counter updated: ", obj)
+          console.log("Before: ", instance.likeCounter);
+
+           ++instance.likeCounter // increment likeCounter by 1
+
+           console.log("After: ", instance.likeCounter);
+
+           instance.save()
+
+            console.log("Done!");
+        });
+      
+        // Not the correct implementation
+        // A user that has not refereshed their device will have a lower like count displayed and would therefore cause 
+        // the likeCounter to become in accurate over time. The value should simply be incremented from the database side 
+        // regardless of what the user sees on their screen at any point and time
+
+        // Passage.upsertWithWhere({id:data.passageId}, {likeCounter: data.currentLikeCounter}, function(err, obj) {
+        //     console.log(err)
+        //     console.log("Like Counter updated: ", obj)
             
-        })
+        // })
 
       }
 
@@ -24,7 +37,8 @@ module.exports = function(Passage) {
               accepts: [
                   {arg: 'data', type: 'object', http: { source: 'body' } }
             ],
-              returns: {arg: 'name', type: 'string'}
+              returns: {arg: 'name', type: 'string'},
+              description: ["increments the likeCounter of a passage by 1"]
             }
         );
 
